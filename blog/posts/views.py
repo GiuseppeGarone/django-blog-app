2e0f9101256476ac4924ddfb81c8be9a1121690d
GiuseppeGarone <giuseppe.garone.1992@gmail.com>
2024-09-08 15:05:57 +0200

    code format

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -3,10 +3,12 @@ from .models import Post
 
 # Create your views here.
 
+
 def index(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'index.html', context)
+
 
 def post(request, pk):
     post = Post.objects.get(id=pk)
